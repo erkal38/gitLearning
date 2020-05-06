2b1b1dab0941df2eb897a88e98fe4e443f3a7576
erkal38 <nalruvceysem1984@gmail.com>
2020-05-06 18:36:03 +0300

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -3,5 +3,6 @@ package tests;
 public class Spring {
     public static void main(String[] args) {
         System.out.println("Let's go outsige.Springe is coming soon");
+        System.out.println();
     }
 }
