4f9b36f600f65d1de724699def40aec313fcbd13
erkal38 <61919635+erkal38@users.noreply.github.com>
2020-05-13 18:01:23 +0300

    Update CreateChannel.java

--- src/tests/CreateChannel.java
+++ src/tests/CreateChannel.java
@@ -4,7 +4,7 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("the weather is very nice.");
         System.out.println("Corona corona dolana dolana ");
-
+      //adfadsfasdfasdf
         if(x==0){
             System.out.println("equal");
         }
