277e9d8dca2254846c79aedcf13ee70b84280e25
erkal38 <nalruvceysem1984@gmail.com>
2020-05-13 17:58:02 +0300

    update CreateChannel.java

---
+++ src/tests/CreateChannel.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+        System.out.println("the weather is very nice.");
+        System.out.println("Corona corona dolana dolana ");
+
+        if(x==0){
+            System.out.println("equal");
+        }
+    }
+}
