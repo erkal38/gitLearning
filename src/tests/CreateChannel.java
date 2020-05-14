079d4fb49b35c3d523fa41781075620e6d66ebc5
erkal38 <nalruvceysem1984@gmail.com>
2020-05-14 23:36:18 +0300

    update Login.java

--- src/tests/CreateChannel.java
+++ src/tests/CreateChannel.java
@@ -5,6 +5,7 @@ public class CreateChannel {
         System.out.println("the weather is very nice.");
         System.out.println("Corona corona dolana dolana ");
       //adfadsfasdfasdf
+        int x = 0;
         if(x==0){
             System.out.println("equal");
         }else{
